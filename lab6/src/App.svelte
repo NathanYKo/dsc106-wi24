ae94a8c36b9f795c0f8d4e9caa80705d3913df72
NathanYKo <knathan774@gmail.com>
2024-02-13 12:26:24 -0800

    Update App.svelte

--- lab6/src/App.svelte
+++ lab6/src/App.svelte
@@ -1,4 +1,13 @@
 <script>
+    mapboxgl.accessToken = "pk.eyJ1IjoibmFrbzEyMyIsImEiOiJjbHNrc3h0d3YwNzgzMmxtdGFydGhuZXJzIn0.T7vvjLMcZf-9iXgnVVAsWQ";
+	const map = new mapboxgl.Map({
+		container: "map",
+		style: "mapbox://styles/mapbox/light-v11", 
+		center: [-71.0942, 42.3601], 
+		zoom: 13, // starting zoom level
+		minZoom: 12,
+		maxZoom: 15,
+	});
 
 </script>
 
